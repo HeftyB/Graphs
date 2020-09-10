f96ddc2ec22748238e675027e64c229f68c98a92
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-09-09 23:26:01 -0400

    day 2 MVP

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,21 @@
+def earliest_ancestor(ancestors, starting_node, current_node=None):
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+    childict = {}
+
+    if current_node is None:
+        current_node = starting_node
+
+    for i in ancestors:
+        if i[1] not in childict:
+            childict[i[1]] = []
+        childict[i[1]].append(i[0])
+
+    if current_node in childict:
+        for i in childict[current_node]:
+            return earliest_ancestor(ancestors, starting_node, i)
+
+    elif current_node is starting_node:
+        return (-1)
+        
+    else:
+        return current_node
--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,42 +13,81 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        q = Queue()
+        v = set()
+
+        q.enqueue(starting_vertex)
+
+        while q.size() > 0:
+            x = q.dequeue()
+
+            if x not in v:
+                print(x)
+                v.add(x)
+
+                for n in self.get_neighbors(x):
+                    q.enqueue(n)
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        s = Stack()
+        v = set()
 
-    def dft_recursive(self, starting_vertex):
+        s.push(starting_vertex)
+
+        while s.size() > 0:
+            x = s.pop()
+
+            if x not in v:
+                print(x)
+                v.add(x)
+
+                for n in self.get_neighbors(x):
+                    s.push(n)
+
+    def dft_recursive(self, starting_vertex, visted = set()):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
 
         This should be done using recursion.
         """
-        pass  # TODO
+        # print(starting_vertex)
+        if visted == self.vertices.keys():
+            print(visited)
+        if starting_vertex not in visted:
+            visted.add(starting_vertex)
+        
+        for i in self.get_neighbors(starting_vertex):
+            if starting_vertex in visted:
+                return 
+            self.dft_recursive(i, visted)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
